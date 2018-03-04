018b07cfbaf5dbf610c24dc4ce09b08e8a51fc36
duncathan <dunc2403@dunc2403.com>
2018-03-04 02:18:42 -0600

    adds the functionality needed to function as a proper HMI

--- src/smashbox/smashbox.ino
+++ src/smashbox/smashbox.ino
@@ -29,6 +29,8 @@
 #define Y_AXIS_1 36
 #define Y_AXIS_2 37
 
+#define D_PAD 43
+
 //SD card
 #define SD_DO  50
 #define SD_DI  51
@@ -46,6 +48,18 @@
 #define STICK_NEUTRAL 128
 #define STICK_MAX     255
 
+#define X_MOD_NONE 86
+#define X_MOD_1    27
+#define X_MOD_2    55
+#define X_MOD_BOTH 73
+
+#define Y_MOD_NONE 86
+#define Y_MOD_1    27
+#define Y_MOD_2    53
+#define Y_MOD_BOTH 74
+
+#define ANALOG_SHOULDER 74
+
 /********GLOBALS********/
 CGamecubeConsole Console(DATA); //creates a virtual console sending and receiving data to/from the DATA pin
 Gamecube_Data_t data = defaultGamecubeData; //initializes the data struct which is sent to console
@@ -62,10 +76,76 @@ void setup() {
 	Serial.begin(9600);
 }
 
+//gamecube reads values 1 and 0, not true and false
+//this function encodes the value of a pin into a gamecube-readable format
+int readButton(int pin) {
+	return (digitalRead(pin) == LOW ? 1 : 0);
+}
+
 void loop() {
+	//data = defaultGamecubeData; //reset all values to default so that the previous cycle's inputs do not carry over
+	/*
 	static int testing = UNPUSHED;
 	data.report.start = ++testing;
     testing %= 2;
+	*/
+
+	/********CONTROL STICK********/
+	data.report.xAxis = STICK_NEUTRAL;
+	if(readButton(LEFT) != readButton(RIGHT)) { //both at the same time is considered neutral
+		int sign = (readButton(LEFT) ? -1 : +1); //subtract modifiers if inputting left; add modifiers if inputting right
+		int mod = 0;
+
+		if(readButton(X_AXIS_1))     mod += X_MOD_1;
+		if(readButton(X_AXIS_2))     mod += X_MOD_2;
+		if(mod == X_MOD_1 + X_MOD_2) mod = X_MOD_BOTH;
+		else if(mod == 0)            mod = X_MOD_NONE;
+
+		data.report.xAxis += mod * sign;
+	}
+
+	data.report.yAxis = STICK_NEUTRAL;
+	if(readButton(UP) != readButton(DOWN)) { //same logic as L/R
+		int sign = (readButton(UP) ? +1 : -1);
+		int mod = 0;
+
+		if(readButton(Y_AXIS_1))     mod += Y_MOD_1;
+		if(readButton(Y_AXIS_2))     mod += Y_MOD_2;
+		if(mod == Y_MOD_1 + Y_MOD_2) mod = Y_MOD_BOTH;
+		else if(mod == 0)            mod = Y_MOD_NONE;
+
+		data.report.yAxis += mod * sign;
+	}
+
+	/********C STICK********/
+	data.report.cxAxis = STICK_NEUTRAL;
+	data.report.cyAxis = STICK_NEUTRAL;
+	if(readButton(C_LEFT) && !readButton(C_RIGHT)) data.report.cxAxis = STICK_MIN
+	if(!readButton(C_LEFT) && readButton(C_RIGHT)) data.report.cxAxis = STICK_MAX
+	if(readButton(C_UP) && !readButton(C_DOWN))    data.report.cyAxis = STICK_MAX
+	if(!readButton(C_UP) && readButton(C_DOWN))    data.report.cyAxis = STICK_MIN
+
+	/********FACE BUTTONS********/
+	data.report.a = readButton(A);
+	data.report.b = readButton(B);
+	data.report.x = readButton(X);
+	data.report.y = readButton(Y);
+
+	data.report.start = readButton(START);
+
+	/********SHOULDER BUTTONS********/
+	data.report.l = readButton(L);
+	data.report.r = readButton(R);
+	data.report.z = readButton(Z);
+	data.report.right = (readButton(R_ANALOG) ? ANALOG_SHOULDER : 0);
+
+	/********D-PAD********/
+	//the modifier buttons double as the D-Pad when a switch on the controller is toggled
+	data.report.dleft  = readButton(D_PAD) & readButton(X_AXIS_1);
+	data.report.dright = readButton(D_PAD) & readButton(Y_AXIS_2);
+	data.report.dup    = readButton(D_PAD) & readButton(Y_AXIS_1);
+	data.report.ddown  = readButton(D_PAD) & readButton(X_AXIS_2);
+
 
 	if(!Console.write(data)) {
 		//uh oh, something went wrong
